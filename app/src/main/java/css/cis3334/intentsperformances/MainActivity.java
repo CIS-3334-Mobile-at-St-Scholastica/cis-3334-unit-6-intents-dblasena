af153dff607bc0a6e6ab92d438b5b150d5641059
dblasena <dblasena@css.edu>
2017-02-24 11:34:48 -0600

    In Class

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +14,10 @@ import java.util.GregorianCalendar;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1Web, but1Calendar, btn1Map;
+    Button btn2web, btn2Calendar, btn2Map;
+    Button btn3web, btn3Calendar, btn3Map;
     TextView textViewStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,21 @@ public class MainActivity extends AppCompatActivity {
         setupButtonClickEvents();
 
 
-
     }
-
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
     private void setupButtonClickEvents() {
         /**
@@ -35,7 +51,28 @@ public class MainActivity extends AppCompatActivity {
         btn1Web = (Button) findViewById(R.id.button1WebInfo);
         btn1Web.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                openWebPage("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d122420167%26view%3devent%26-childview%3d");
+                textViewStatus.setText("Code should display website for performance 1");
+            }
+        });
+
+
+
+
+
+        btn2web = (Button) findViewById(R.id.btn2web);
+        btn2web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 // Do something in response to button click
+                openWebPage("http://www.css.edu/About/Spotlight-Arts-and-Lectures/Calendar.html?trumbaEmbed=eventid%3D119983186%26view%3Devent%26-childview%3D");
+                textViewStatus.setText("Code should display website for performance 1");
+            }
+        });
+        btn3web = (Button) findViewById(R.id.btn3web);
+        btn3web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                openWebPage("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d119983357%26view%3devent%26-childview%3d");
                 textViewStatus.setText("Code should display website for performance 1");
             }
         });
@@ -63,20 +100,81 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        /**
-         *   Set up button click event listener for the web info button for the first performance
-         */
-        btn1Map = (Button) findViewById(R.id.button1map);
-        btn1Map.setOnClickListener(new View.OnClickListener() {
+        btn2Calendar = (Button) findViewById(R.id.btn2calendar);
+        btn2Calendar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                textViewStatus.setText("Code should display map for performance 1");
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "CSS Choirs At Home");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 3, 2, 19, 30);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 3, 2, 21, 30);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
             }
         });
+        btn3Calendar = (Button) findViewById(R.id.btn3calendar);
+        btn3Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "The Secret Life of Children");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 3, 4, 19, 30);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 3, 4, 21, 30);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
 
+                /**
+                 *   Set up button click event listener for the web info button for the first performance
+                 */
+                btn1Map = (Button) findViewById(R.id.button1map);
+                btn1Map.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        showMap("geo:46.8179174,-92.1022524");
 
-    }
+                        textViewStatus.setText("Code should display map for performance 1");
+                    }
+                });
+                btn2Map = (Button) findViewById(R.id.button2map);
+                btn2Map.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        showMap("geo:46.8179174,-92.1022524");
+                        textViewStatus.setText("Code should display map for performance 1");
+                    }
+                });
+                btn3Map = (Button) findViewById(R.id.button3map);
+                btn3Map.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        showMap("geo:46.8179174,-92.1022524");
+                        textViewStatus.setText("Code should display map for performance 1");
+                    }
+                });
+            }
 
-
+            ;
+        });
+    };
 }
+
+
+
+
+
+
+
+
